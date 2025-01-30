06866368131eca39ee5c2085a74b1be8ae70e7e3
Matthew Elliott <m.elliott091091@gmail.com>
2025-01-30 10:51:27 +0000

    Added validation to model , created a form instead of labels and inputs for shopping list.

--- FirstBlazorApp/Data/Grocery.cs
+++ FirstBlazorApp/Data/Grocery.cs
@@ -1,8 +1,14 @@
-﻿namespace FirstBlazorApp.Data
+﻿using System.ComponentModel.DataAnnotations;
+namespace FirstBlazorApp.Data
+
 {
     public class Grocery
     {
+        [Required]
+        [StringLength(20, ErrorMessage = "Name should be less than 20 characters")]
         public string Name { get; set; }
+        [Required]
+        [Range(1, 10000, ErrorMessage = "Price should be between 1 and 10,000")]
         public float Price { get; set; }
     }
 }
